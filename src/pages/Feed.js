b037e358fec5756ef26191f14d2b38bbd5711a12
Linus Haren <linus@haren.io>
2023-04-13 15:14:05 +0200

    add temporary logout button

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,10 +1,32 @@
 import CreatePost from '../components/CreatePost';
 import GetAllPosts from '../components/GetAllPosts';
+import { useAuth } from '../context/AuthContext';
 import '../styles/Feed.scss';
 
+
+
 const Feed = () => {
+
+  //Tillfällig logout knapp och namnbricka
+  const { setLoggedIn, setUser, user } = useAuth();
+  const handleLogout = async () => {
+    try {
+      const res = await fetch('http://localhost:5050/auth/logout', {
+        credentials: 'include',
+      });
+      if (res.ok) {
+        setLoggedIn(false);
+        setUser(null)
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
+      <button onClick={handleLogout}>Logout</button>
+      <p>Logged in as: {user.username}</p>
       <h1>Social media app Feed</h1>
       <CreatePost />
       <GetAllPosts />
