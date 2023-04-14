cbefb33d01a1d0faf068217a83b5f3ace46d74d8
Linus Haren <linus@haren.io>
2023-04-14 14:00:08 +0200

    add Post component. contains all necessary info about post

---
+++ src/components/Post.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import '../styles/Post.scss';
+import { Link } from 'react-router-dom'
+import LikeButton from './LikeButton'
+import CommentButton from './CommentButton'
+import { useAuth } from '../context/AuthContext';
+
+function Post({ post }) {
+
+  const { body, comments, date, like, userID, _id } = post
+  const { user } = useAuth();
+  const commentCount = comments.length
+  const likeCount = like.length
+  return (
+    <div className="post-card">
+      <div className="post-header">
+        <Link className="post-title" to={`../profile/${userID}`}>@{userID}</Link>
+        {userID === user.username ? <button>Delete/Edit</button> : null}
+      </div>
+      <p className="post-body">{body}</p>
+      <div className="post-stats">
+        <p className="like-count">{likeCount} likes</p>
+        <p className="comment-count">{commentCount} comments</p>
+      </div>
+      <div className="post-buttons">
+        <LikeButton className="like-button" post={post} />
+        <CommentButton className="comment-button" post={post} />
+      </div>
+      <div className="comment-section" style={{ display: 'none' }}>
+        {/* map through comments and render them here ? */}
+      </div>
+    </div >
+  )
+}
+
+export default Post
