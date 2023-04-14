4a6bfb16038c35a9232adfba0d0775c17a70431c
Linus Haren <linus@haren.io>
2023-04-14 13:58:45 +0200

    add like button component

---
+++ src/components/LikeButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LikeButton({ post }) {
+  return (
+    <button onClick={() => console.log("ouch")}>
+      Like
+    </button>
+  )
+}
+
+export default LikeButton
