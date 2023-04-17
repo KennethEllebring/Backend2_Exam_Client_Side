5a794b24027246da127b6396051ba4ae5de7ac76
AlexSaxena <saxenaalex@gmail.com>
2023-04-17 18:42:25 +0200

    Updates Component W. fetch req to DELETE post

--- src/components/DeletePostComp.js
+++ src/components/DeletePostComp.js
@@ -1,7 +1,16 @@
-import React from "react";
-
-function DeletePostComp() {
-  return <button onClick={() => console.log("Removed")}>Delete</button>;
+async function DeletePostComp(id) {
+  const confirmBox = window.confirm("Do you Want To Remove Post");
+  if (confirmBox) {
+    await fetch("http://localhost:5050/posts", {
+      method: "DELETE",
+      body: JSON.stringify({ id }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    window.location.reload();
+  }
 }
 
 export default DeletePostComp;
