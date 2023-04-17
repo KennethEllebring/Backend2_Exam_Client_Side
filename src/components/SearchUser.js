13888cfac6adfcd61830a84a03c4b6c796673007
Linus Haren <linus@haren.io>
2023-04-17 12:40:54 +0200

    add a component used for user searching. Has auto-complete functionality

---
+++ src/components/SearchUser.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import FollowButton from './FollowButton';
+import '../styles/SearchUser.scss';
+
+function SearchUser() {
+
+  const [searchTerm, setSearchTerm] = useState('')
+  const [userList, setUserList] = useState([])
+  const [searchResult, setSearchResult] = useState([])
+  const navigate = useNavigate()
+
+  useEffect(() => {
+
+    const fetchUsers = async () => {
+      const response = await fetch('http://localhost:5050/users/all', {
+        credentials: 'include'
+      })
+      if(!response.ok){
+        throw new Error('Error fetching users')
+      }
+      const users = await response.json()
+      setUserList(users.users)
+    }
+    fetchUsers()
+  }, [])
+
+  useEffect(() => {
+    if (searchTerm === '') {
+      return setSearchResult([])
+    }
+    const result = userList.filter((user) => {
+      return user.includes(searchTerm.toLowerCase())
+    })
+    setSearchResult(result)
+  }, [searchTerm, userList])
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  const renderedUsers = searchResult.slice(0, 10).map((user) => {
+    return (
+      <div key={user} className="userItem">
+        <li onClick={(() => navigate(`../profile/${user}`))}>{user}</li>
+        <FollowButton user={user} className="followBtn" />
+      </div>
+    )
+  })
+
+
+
+  return (
+    <div className="searchArea">
+      <h2>Search for user</h2>
+      <input
+        type="text"
+        className="searchInput"
+        placeholder="Search for users.."
+        onChange={handleChange}
+      />
+      <ul className="searchList">
+        {renderedUsers}
+      </ul>
+    </div>
+  )
+}
+
+export default SearchUser
