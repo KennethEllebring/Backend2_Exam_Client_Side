e35756e7f7fa256913e790b94b18572f050fdaa2
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-04-18 11:02:43 +0200

    added button logic and button changer like/unlike

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import {toast} from 'react-toastify';
 
-function LikeButton({ post }) {
-  return (
-    <button onClick={() => console.log("ouch")}>
-      Like
-    </button>
-  )
+async function likePost(id) {
+  console.log(id);
+
+  try {
+    const response = await fetch('http://localhost:5050/posts/like', {
+      method: 'PATCH',
+      body: JSON.stringify({id}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+    const data = await response.json();
+    toast.success(data.message);
+    window.location.reload();
+  } catch (error) {
+    toast.error(error.message);
+  }
 }
 
-export default LikeButton
+function LikeButton({post}) {
+  return post[0].like.includes(post[1].username) ? <button onClick={() => likePost(post[0]._id)}>Unlike</button> : <button onClick={() => likePost(post[0]._id)}>Like</button>;
+}
+
+export default LikeButton;
