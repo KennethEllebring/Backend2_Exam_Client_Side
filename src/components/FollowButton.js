957a569ea906fd26f265a1dcc93b604860c84769
Linus Haren <linus@haren.io>
2023-04-19 11:42:22 +0200

    add fetch for logged in user to get following list

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAuth } from '../context/AuthContext';
 
-function FollowButton({user}) {
-  console.log(user)
+
+function FollowButton({ profile }) {
+
+  const { user } = useAuth();
+  const [userFollowing, setUserFollowing] = useState([])
+
+  const checkLogin = async () => {
+    try {
+      const response = await fetch(`http://localhost:5050/users/single/${user.username}`, {
+        credentials: 'include'
+      })
+      const data = await response.json()
+      if (data) {
+        setUserFollowing(data.following)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+
+  }
+
+
+  useEffect(() => {
+    checkLogin()
+  }, [])
+
+
+
+  const isFollowing = userFollowing.includes(profile.username)
+  
+  console.log(isFollowing)
+
   return (
     <button>Follow</button>
   )
