c7ac3df723b53d04fa7ee8a10bea2e445037ff1f
Linus Haren <linus@haren.io>
2023-04-19 11:22:21 +0200

    add user fetch for displaying more info about user

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,30 +1,38 @@
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from 'react'
 import Post from "../components/Post";
+import FollowButton from "../components/FollowButton";
 
 const Profile = () => {
 
   const { username } = useParams()
-  const [userPosts, setUserPosts] = useState([])
+  const [posts, setPosts] = useState([])
+  const [user, setUser] = useState({})
   const [loading, setLoading] = useState(true)
-
+  const [followingCount, setFollowingCount] = useState(null)
+  
   const getUser = async () => {
     try {
-      const response = await fetch(`http://localhost:5050/posts/all/user/${username}`, {
-        credentials: 'include'
-      })
-      if (!response.ok) {
-        throw new Error('Failed to fetch user posts')
-      }
+      const postPromise = fetch(`http://localhost:5050/posts/all/user/${username}`, {credentials: 'include'})
+      const userPromise = fetch(`http://localhost:5050/users/single/${username}`, {credentials: 'include'})
 
-      const data = await response.json()
-      const flattenedPosts = data.flat();
+      const results = await Promise.all([postPromise, userPromise])
+      const postData = await results[0].json()
+      const userData = await results[1].json()
+      if(userData.message === 'User not found'){
+        setUser(null)
+        throw new Error(userData.message)
+      }
+      if(postData.message === 'No posts were found') {
+        throw new Error(postData.message)
+      }
+      const flattenedPosts = postData.flat();
       const sortedPosts = flattenedPosts.sort((a, b) => new Date(b.date) - new Date(a.date));
-      setUserPosts(sortedPosts)
+      setPosts(sortedPosts)
+      setUser(userData)
       setLoading(false)
     } catch (error) {
       setLoading(false)
-      console.error(error)
     }
   }
 
@@ -33,13 +41,14 @@ const Profile = () => {
     getUser()
   }, [])
 
-  const renderedPosts = userPosts.map((post) => {
+
+  const renderedPosts = posts.map((post) => {
     return <Post key={post._id} post={post} />
   })
 
-  if (loading) return null
+  if(loading) return null
 
-  if (userPosts.length === 0) {
+  if (user === null) {
     return (
       <div>
         <Link to="../feed">Back to feed</Link>
@@ -49,8 +58,12 @@ const Profile = () => {
   } return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem', width: '500px', maxWidth: '90%', border: '1px solid red', alignItems: 'center' }}>
       <Link to="../feed">Back to feed</Link>
-      <h1>@{username}</h1>
-      {renderedPosts}
+      <div className="profile-header">
+        <h1>@{username}</h1>
+        <FollowButton user={user}/>
+        <p>Following: {user.following ? user.following.length : 0}</p>
+      </div>
+      {posts.length ? renderedPosts : <p>This user has no posts yet</p>}
     </div>
   )
 
