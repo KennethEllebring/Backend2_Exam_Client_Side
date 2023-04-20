8787185ef280eb7308bed79e1c0d5b17fdf29a8b
Linus Haren <linus@haren.io>
2023-04-20 13:42:50 +0200

    add follower count and subtract 1 to account for self-follow

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -9,16 +9,16 @@ const Profile = () => {
   const [posts, setPosts] = useState([])
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
-  
+
   const getUser = async () => {
     try {
-      const postPromise = fetch(`http://localhost:5050/posts/all/user/${username}`, {credentials: 'include'})
-      const userPromise = fetch(`http://localhost:5050/users/single/${username}`, {credentials: 'include'})
+      const postPromise = fetch(`http://localhost:5050/posts/all/user/${username}`, { credentials: 'include' })
+      const userPromise = fetch(`http://localhost:5050/users/single/${username}`, { credentials: 'include' })
 
       const results = await Promise.all([postPromise, userPromise])
       const postData = await results[0].json()
       const userData = await results[1].json()
-      if(userData.message === 'User not found'){
+      if (userData.message === 'User not found') {
         setUser(null)
       } else {
         setUser(userData)
@@ -42,7 +42,7 @@ const Profile = () => {
     return <Post key={post._id} post={post} />
   })
 
-  if(loading) return null
+  if (loading) return null
 
   if (user === null) {
     return (
@@ -51,13 +51,13 @@ const Profile = () => {
         <h1>Profile not found</h1>
       </div>
     )
-  }console.log(user) 
+  } console.log(user)
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem', width: '500px', maxWidth: '90%', border: '1px solid red', alignItems: 'center' }}>
       <Link to="../feed">Back to feed</Link>
       <div className="profile-header">
         <h1>@{username}</h1>
-        <FollowButton profile={user}/>
+        <FollowButton profile={user} />
         <p>Following:  {user.following.length - 1}</p>
         <p>Followers:  {user.followers.length - 1}</p>
 
