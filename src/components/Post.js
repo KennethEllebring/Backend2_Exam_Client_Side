e02ce72f857278c57eb1897e7c97b9cec58994c8
AlexSaxena <saxenaalex@gmail.com>
2023-04-26 09:36:00 +0200

    Removes not in use variables

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
-import '../styles/Post.scss';
-import { Link } from 'react-router-dom';
-import LikeButton from './LikeButton';
-import Comment from './Comment';
-import DeletePostComp from './DeletePostComp';
-import { useAuth } from '../context/AuthContext';
-import EditPostModal from './EditPostModal';
+import "../styles/Post.scss";
+import { Link } from "react-router-dom";
+import LikeButton from "./LikeButton";
+import Comment from "./Comment";
+import DeletePostComp from "./DeletePostComp";
+import { useAuth } from "../context/AuthContext";
+import EditPostModal from "./EditPostModal";
 
 function Post({ post }) {
   const { body, comments, date, like, userID, _id } = post;
@@ -14,40 +13,36 @@ function Post({ post }) {
   const likeCount = like.length;
 
   return (
-    <div className='post-card'>
-      <div className='post-header'>
-        <Link className='post-title' to={`../profile/${userID}`}>
+    <div className="post-card">
+      <div className="post-header">
+        <Link className="post-title" to={`../profile/${userID}`}>
           @{userID}
         </Link>
         {userID === user.username ? (
-          <div className='header-buttons'>
-            <EditPostModal className='main-button' post={post} />
-            <DeletePostComp className='delete-button' id={_id} />
+          <div className="header-buttons">
+            <EditPostModal className="main-button" post={post} />
+            <DeletePostComp className="delete-button" id={_id} />
           </div>
         ) : null}
       </div>
 
-      <p className='post-body'>{body}</p>
+      <p className="post-body">{body}</p>
 
-      <div className='post-stats'>
-        <p className='like-count'>
-          {likeCount === 1
-            ? `${likeCount} like`
-            : `${likeCount} likes`}
+      <div className="post-stats">
+        <p className="like-count">
+          {likeCount === 1 ? `${likeCount} like` : `${likeCount} likes`}
         </p>
-        <p className='comment-count'>
+        <p className="comment-count">
           {commentCount === 1
             ? `${commentCount} comment`
             : `${commentCount} comments`}
         </p>
       </div>
-      <div className='post-buttons'>
+      <div className="post-buttons">
         <LikeButton post={[post, user]} />
-        <Comment className='comment-button' post={post} />
+        <Comment className="comment-button" post={post} />
       </div>
-      <div className='comment-section' style={{ display: 'none' }}>
-        {/* map through comments and render them here ? */}
-      </div>
+      <div className="comment-section" style={{ display: "none" }}></div>
     </div>
   );
 }
